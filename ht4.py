1a314fa3a9fd3b5e432b9998951e18dc95cab45f
serhSK <sergeyskotnitskiy@gmail.com>
2017-11-15 04:04:05 +0200

    added universal function get_messages

--- ht4.py
+++ ht4.py
@@ -48,4 +48,19 @@ def get_criticals():
     critical_dict.update({"DateTime":dt_tm_list})
     critical_dict.update({"Text":text_list})
     return critical_dict
-print(get_criticals())
+
+def get_messages(type, pattern):
+    result = {"Marker": type}
+    dt_tm_list = []
+    text_list = []
+    for line in lines:
+        l_mess = re.search(pattern, line)
+        l_date = re.search(date_time_ptrn, line)
+        if l_mess is not None:
+            dt_tm_list.append(l_date.group())
+            text_list.append(l_mess.group().split(type)[1])
+    result.update({"DateTime":dt_tm_list})
+    result.update({"Text":text_list})
+    return result
+print((get_messages("ERROR", errors_ptrn)))
+
