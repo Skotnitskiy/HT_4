f967972e10c32a7cb6a059d00614ed5e3d3d6638
serhSK <sergeyskotnitskiy@gmail.com>
2017-11-15 03:47:43 +0200

    added get_warnings function

--- ht4.py
+++ ht4.py
@@ -21,4 +21,17 @@ def get_errors():
     err_dict.update({"Text":text_list})
     return err_dict
 
-print(get_errors())
+def get_warnings():
+    warn_dict = {"Marker": "WARNING"}
+    dt_tm_list = []
+    text_list = []
+    for line in lines:
+        l_warn = re.search(warnings_ptrn, line)
+        l_date = re.search(date_time_ptrn, line)
+        if l_warn is not None:
+            dt_tm_list.append(l_date.group())
+            text_list.append(l_warn.group().split("WARNING")[1])
+    warn_dict.update({"DateTime":dt_tm_list})
+    warn_dict.update({"Text":text_list})
+    return warn_dict
+print(get_warnings())
