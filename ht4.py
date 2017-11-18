c2d9a264268bf3183aab827f074562fb640bde6d
serhSK <sergeyskotnitskiy@gmail.com>
2017-11-18 17:28:23 +0200

    altered and fixed bugs

--- ht4.py
+++ ht4.py
@@ -1,7 +1,6 @@
-import re
 import csv
-
 import os
+import re
 
 # create dir
 rep_path = "../reports/"
@@ -16,36 +15,31 @@ all_data_csv_path = rep_path + "all_data.csv"
 unique_csv_path = rep_path + "unique.csv"
 lines = f_log.readlines()
 
-markers_list = ["WARNING", "ERROR", "CRITICAL"]
 date_time_ptrn = "[0-9]+-\d\d-\d\d \d\d:\d\d:\d\d"
-warnings_ptrn = markers_list[0] + "(.*)"
-errors_ptrn = markers_list[1] + "(.*)"
-criticals_ptrn = markers_list[2] + "(.*)"
+log_ptrn = "(WARNING|CRITICAL|ERROR)(.*)"
+marker_ptrn = "(WARNING|CRITICAL|ERROR)"
 
-patterns_list = [warnings_ptrn, errors_ptrn, criticals_ptrn]
 csv_all_data_columns = ["LineId", "Marker", "DateTime", "Description"]
 csv_unique_data_columns = ["Count", "Marker", "DateTime", "Description"]
 
-dtm_list = []
-t_list = []
-m_list = []
+dscr_dict = {}  # description_dict
+dscr_list = []  # description_list
 
 
-def get_messages(message_type, log_pattern):
+def get_messages(log_pattern):
     result = []
     line_count = 0
     for line in lines:
         l_mess = re.search(log_pattern, line)
         l_date = re.search(date_time_ptrn, line)
-        line_count += 1
+        l_marker = re.search(marker_ptrn, line)
         if l_mess is not None:
-            result.append({csv_all_data_columns[0]: line_count})
-            result.append({csv_all_data_columns[1]: message_type})
-            result.append({csv_all_data_columns[2]: l_date.group().strip()})
-            result.append({csv_all_data_columns[3]: l_mess.group().split(message_type)[1].strip()})
-            m_list.append(message_type)
-            dtm_list.append(l_date.group().strip())
-            t_list.append(l_mess.group().split(message_type)[1].strip())
+            line_count += 1
+            result.append({"LineId": line_count, "Marker": l_marker.group(), "DateTime": l_date.group().strip(),
+                           "Description":  l_mess.group().split(l_marker.group())[1]})
+            dscr_dict.update({l_mess.group().split(l_marker.group())[1]: [l_marker.group(), l_date.group().strip()]})
+            dscr_list.append(l_mess.group().split(l_marker.group())[1])
+
     return result
 
 
@@ -56,45 +50,17 @@ def write_dict_to_csv(csv_file, csv_columns, dict_data):
         writer.writerows(dict_data)
 
 
-messages_list = []
-for marker, pattern in zip(markers_list, patterns_list):
-    messages_list.append(get_messages(marker, pattern))
+# write to file all_data
+write_dict_to_csv(all_data_csv_path, csv_all_data_columns, get_messages(log_ptrn))
 
-# write to file all data
-for message in messages_list:
-    write_dict_to_csv(all_data_csv_path, csv_all_data_columns, message)
-
-unique_descriptions = []
 unique_result = []
+description_count = 0
+for deskription, key in dscr_dict.items():
+    for ds in dscr_list:
+        if deskription == ds:
+            description_count += 1
+    unique_result.append({"Count": description_count, "Marker": key[0], "DateTime": key[1], "Description": deskription})
+    description_count = 0
 
-unique_warn_count = 0
-unique_err_count = 0
-unique_crit_count = 0
-
-duplicate_warn_count = 0
-duplicate_err_count = 0
-duplicate_crit_count = 0
-
-all_data_count = 0
-
-for marker, date_time, description in zip(m_list, dtm_list, t_list):
-    if description not in unique_descriptions:
-        unique_descriptions.append(description)
-        if marker == "WARNING":
-            unique_warn_count += 1
-        elif marker == "ERROR":
-            unique_err_count += 1
-        elif marker == "CRITICAL":
-            unique_crit_count += 1
-        unique_result.append({csv_unique_data_columns[0]: "*"})
-        unique_result.append({csv_unique_data_columns[1]: marker})
-        unique_result.append({csv_unique_data_columns[2]: date_time})
-        unique_result.append({csv_unique_data_columns[3]: description})
-    all_data_count += 1
-
-
+# write to file unique data
 write_dict_to_csv(unique_csv_path, csv_unique_data_columns, unique_result)
-
-print("Unique:", len(unique_descriptions), "warnings:", unique_warn_count,
-      "errors:", unique_err_count, "criticals:", unique_crit_count)
-print("All data:", all_data_count)
